768b68fa7865526a136354ae261f0ba12c12601f
HaniAlHawasli <74898484+HaniAlHawasli@users.noreply.github.com>
2021-06-12 14:10:59 +0200

    Update readingCMOS.py

--- readingCMOS.py
+++ readingCMOS.py
@@ -1,9 +1,12 @@
-class readingCMOS():
-    import h5py
-    import numpy as np
+import h5py
+import numpy as np
 
-    @staticmethod
-    def h5printR(item, leading=''):
+class readingCMOS():
+
+super(Opening, self).__init__()
+
+  @staticmethod
+   def h5printR(item, leading=''):
         for key in item:
             if isinstance(item[key], h5py.Dataset):
                 print(leading + key + ' : ' + str(item[key].shape))
@@ -12,10 +15,11 @@ class readingCMOS():
                 h5printR(item[key], leading + ' ')
 
     @staticmethod
+    @lru_cache
     def h5print(filename):
         with h5py.File(filename, 'r') as h:
             print(filename)
-            h5printR('h', '  ')
+            h5printR(h, '  ')
 
 
-readingCMOS.h5print('filename.bxr')  # read the structure of .bxr file
+readingCMOS.h5print('filename.bxr')  # read the structure of .bxr fil
