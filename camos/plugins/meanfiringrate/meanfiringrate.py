05e13fc303b4847a1dd7497813f4737adbb8c379
jseidel5 <josuaseidel@gmail.com>
2021-07-09 16:29:18 +0200

    MFR plugin now only needs dataset

--- camos/plugins/meanfiringrate/meanfiringrate.py
+++ camos/plugins/meanfiringrate/meanfiringrate.py
@@ -10,14 +10,12 @@ from camos.tasks.analysis import Analysis
 from camos.utils.generategui import (
     DatasetInput,
     NumericInput,
-    ImageInput,
-    CustomComboInput,
 )
-from camos.utils.units import length
 
 
 class MeanFiringRate(Analysis):
     analysis_name = "Mean Firing Rate"
+    required = ["dataset"]
 
     def __init__(self, model=None, parent=None, signal=None):
         super(MeanFiringRate, self).__init__(
@@ -28,63 +26,62 @@ class MeanFiringRate(Analysis):
 
     def _run(
         self,
-        duration: NumericInput("Total Duration (s)", 100),
-        scale: NumericInput("Axis scale", 1),
-        _i_units: CustomComboInput(list(length.keys()), "Axis units", 0),
+        duration: NumericInput("Recording duration in seconds", 600),
+        electrode_x: NumericInput("Number of electrodes along one axis",64),
+        #scale: NumericInput("Axis scale", 1),
+        #_i_units: CustomComboInput(list(length.keys()), "Axis units", 0),
         _i_data: DatasetInput("Source Dataset", 0),
-        _i_mask: ImageInput("Mask image", 0),
+
     ):
         output_type = [("CellID", "int"), ("MFR", "float")]
         self.duration = duration
-        self.scale = scale
-        self.units = length[list(length.keys())[_i_units]]
+        self.electrode_n = electrode_x
 
-        # data should be provided in format of peaks
+        '''
+        Data format:
+
+        First column is electrode ID
+        Second column is time of event
+            ('CellID', 'Active')
+            [[(1520, 2.64562191e-03)]
+            [(4038, 5.58520180e-03)]
+            [(3245, 6.39358627e-03)]
+        
+        If electrode 1520 has 500 spike events, then there are 500 rows with ID 1520
+        '''
+
         data = self.signal.data[_i_data]
         self.dataname = self.signal.names[_i_data]
-        self.mask = self.model.images[_i_mask].image(0)
-        if not ("Active" in data.dtype.names):
-            return
-
-        ROIs = np.unique(data[:]["CellID"])
 
         # Calculate mean firing rate per cell
+        ROIs = np.unique(data[:]["CellID"])
         self.output = np.zeros(shape=(len(ROIs), 1), dtype=output_type)
+
+        #get the event counts and make CellID unique
         unique, counts = np.unique(data[:]["CellID"], return_counts=True)
         self.output[:]["CellID"] = unique.reshape(-1, 1)
         self.output[:]["MFR"] = counts.reshape(-1, 1)
+        self.output[:]["MFR"] = self.output[:]["MFR"]/self.duration
 
     def output_to_signalmodel(self):
         self.parent.signalmodel.add_data(
-            self.output, "MFR of {}".format(self.dataname), self, mask=self.mask
-        )
+            self.output, "MFR of {}".format(self.dataname), self)
 
     def _plot(self):
-        import matplotlib.ticker as tick
+        mfr = np.zeros(shape=(self.electrode_n * self.electrode_n))
 
-        mask = self.mask.astype(int)
-        MFR_dict = {}
-        for i in range(1, self.foutput.shape[0]):
-            MFR_dict[int(self.foutput[i]["CellID"][0])] = self.foutput[i]["MFR"][0]
+        for idx,i in enumerate(self.output[:]["CellID"]):
+            mfr[i] = self.output[idx]["MFR"]
 
-        k = np.array(list(MFR_dict.keys())).astype(int)
-        v = np.array(list(MFR_dict.values()))
-
-        dim = int(max(k.max(), np.max(mask)))
-        mapping_ar = np.zeros(dim + 1, dtype=v.dtype)
-        mapping_ar[k] = v
-        MFR_mask = mapping_ar[mask]
-
-        self.outputimage = MFR_mask
+        # reshape mfr values in shape of chip for heatmap 
+        mfr = np.reshape(mfr,(self.electrode_n,self.electrode_n))
 
         im = self.plot.axes.imshow(
-            MFR_mask / self.duration, cmap="inferno", origin="upper"
+            mfr, cmap="inferno", origin="upper"
         )
+
         self.plot.fig.colorbar(
-            im, ax=self.plot.axes, label="Mean Firing Rate (Events/s)"
-        )
-        formatter = lambda x, pos: f"{(x / self.scale):.1f}"  # scale is the resolution
-        self.plot.axes.xaxis.set_major_formatter(tick.FuncFormatter(formatter))
-        self.plot.axes.yaxis.set_major_formatter(tick.FuncFormatter(formatter))
-        self.plot.axes.set_ylabel("Y coordinate ({})".format(self.units))
-        self.plot.axes.set_xlabel("X coordinate ({})".format(self.units))
+            im, ax=self.plot.axes, label="Mean Firing Rate (Events/s)")
+
+        self.plot.axes.set_ylabel("Electrode in Y direction")
+        self.plot.axes.set_xlabel("Electrode in X direction")
